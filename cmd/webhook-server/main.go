5f34e4e8262e094a1459f3fc5aee55c91044e66f
peiqi.shi <peiqi.shi@huifu.com>
2022-04-18 11:41:50 +0800

    fix

---
+++ cmd/webhook-server/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"github.com/bretagne-peiqi/oversale-solution-2/cmd/config"
+	"fmt"
+	"k8s.io/api/admission/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+const (
+	tlsDir         = `/run/secrets/tls`
+	tlsCertFile    = `cert.pem`
+	tlsKeyFile     = `key.pem`
+	toolAnnotation = `webhook.citiccard.com`
+)
+
+var (
+	nodeResource = metav1.GroupVersionResource{Version: "v1", Resource: "nodes"}
+)
+
+func getPatchItem(op string, path string, val interface{}) patchOperation {
+	return patchOperation{
+		Op:    op,
+		Path:  path,
+		Value: val,
+	}
+}
+
+func initPatch(node corev1.Node) []patchOperation {
+	var patches []patchOperation
+	origin := node.Status.Allocatable.Cpu().Value()
+	var coff int64 = 1.2
+	fixed := origin*coff
+	patches = append(patches, getPatchItem("replace", "/status/allocatable/cpu", fixed))
+	return patches
+}
+
+func applyNodeConfig(req *v1beta1.AdmissionRequest, toolConfig *config.ToolConfig) ([]patchOperation, error) {
+	if req.Resource != nodeResource {
+		log.Printf("expect resource to be %s", nodeResource)
+		return nil, nil
+	}
+	// Parse the Node object.
+	raw := req.Object.Raw
+	node := corev1.Node{}
+	if _, _, err := universalDeserializer.Decode(raw, nil, &node); err != nil {
+		return nil, fmt.Errorf("could not deserialize node object: %v", err)
+	}
+	var patches []patchOperation
+	patches = initPatch(node)
+	return patches, nil
+}
+func main() {
+	certPath := filepath.Join(tlsDir, tlsCertFile)
+	keyPath := filepath.Join(tlsDir, tlsKeyFile)
+	toolConfig := config.NewToolConfig()
+	mux := http.NewServeMux()
+	log.Printf("listen on port 8443")
+	mux.Handle("/mutate", admitFuncHandler(applyNodeConfig, &toolConfig))
+	server := &http.Server{
+		// We listen on port 8443 such that we do not need root privileges or extra capabilities for this server.
+		// The Service object will take care of mapping this port to the HTTPS port 443.
+		Addr:    ":8443",
+		Handler: mux,
+	}
+	log.Fatal(server.ListenAndServeTLS(certPath, keyPath))
+}
